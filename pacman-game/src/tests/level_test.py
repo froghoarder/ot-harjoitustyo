58d80272817038d2bc3a7328dff14e4778a76dd3
Nea <neazahorszky@gmail.com>
2025-04-08 22:29:12 +0300

    Add tests

--- pacman-game/src/tests/level_test.py
+++ pacman-game/src/tests/level_test.py
@@ -1,47 +1,13 @@
 import unittest
 import pygame
 from level import Level
-from gameloop import GameLoop
 
 LEVEL_MAP_TEST = [[1, 1, 1, 1, 1],
-                  [1, 0, 0, 0, 1],
-                  [1, 3, 0, 0, 1],
+                  [1, 0, 0, 2, 1],
+                  [1, 3, 2, 0, 1],
                   [1, 1, 1, 1, 1]]
 
 CELL_SIZE = 50
-
-
-class StubRenderer:
-    """not relevant yet
-    """
-
-    def render(self):
-        pass
-
-
-class StubClock:
-    """not relevant yet
-    """
-
-    def tick(self, fps):
-        pass
-
-    def get_ticks(self):
-        0
-
-
-class StubEvent:
-    def __init__(self, event_type, key):
-        self.type = event_type
-        self.key = key
-
-
-class StubEventQueue:
-    def __init__(self, events):
-        self._events = events
-
-    def get(self):
-        return self._events
 
 
 class TestLevel(unittest.TestCase):
@@ -55,9 +21,16 @@ class TestLevel(unittest.TestCase):
     def test_can_move_character(self):
 
         self.assert_coordinates_equal(
-            self.level.frog, CELL_SIZE, 2 * CELL_SIZE)
+            self.level.frog, CELL_SIZE, 2 * CELL_SIZE + CELL_SIZE)
 
         self.level.move_frog(1 * CELL_SIZE, -1 * CELL_SIZE)
 
         self.assert_coordinates_equal(
-            self.level.frog, 2 * CELL_SIZE, CELL_SIZE)
+            self.level.frog, 2 * CELL_SIZE, CELL_SIZE + CELL_SIZE)
+
+    def test_can_grow_amount_of_points(self):
+        self.assertEqual(self.level.points, 0)
+        self.level.move_frog(dx=CELL_SIZE)
+        self.level.collect_stuff()
+        self.assertEqual(self.level.points, 1)
+
